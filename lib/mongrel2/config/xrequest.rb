74746de3d455a8322cf699b7442048977f74a1b4
Michael Granger <ged@FaerieMUD.org>
2013-09-18 11:58:00 -0700

    Add support for 1.8.1's "X-Requests"

---
+++ lib/mongrel2/config/xrequest.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+require 'tnetstring'
+
+require 'mongrel2' unless defined?( Mongrel2 )
+require 'mongrel2/config' unless defined?( Mongrel2::Config )
+
+# Mongrel2 X-Request configuration class
+#
+#   # Using the config DSL:
+#   xrequest '/usr/local/lib/mongrel2/filters/watermark.so',
+#       extensions: ['*.jpg', '*.png'],
+#       src: '/usr/local/var/image/acme.png'
+#
+#   # Which is the same as:
+#   Mongrel2::Config::XRequest.create(
+#       name: '/usr/local/lib/mongrel2/filters/sendfile.so',
+#       settings: {
+#         min_size: 1000
+#       }
+#
+#   # Or:
+#   server.add_xrequest(
+#       name: '/usr/local/lib/mongrel2/filters/sendfile.so',
+#       settings: {
+#         min_size: 1000
+#       })
+#
+class Mongrel2::Config::XRequest < Mongrel2::Config( :xrequest )
+
+	### As of Mongrel2/1.8.1:
+	# CREATE TABLE xrequest (id INTEGER PRIMARY KEY,
+	#     server_id INTEGER,
+	#     name TEXT,
+	#     settings TEXT);
+	#
+
+	many_to_one :server
+
+
+	# Serialize the settings column as TNetStrings
+	plugin :serialization, :tnetstring, :settings
+
+end # class Mongrel2::Config::XRequest
+
